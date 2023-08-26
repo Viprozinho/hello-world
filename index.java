90bfc574241048dd3416c5df382d4f61bef9028e
Victor <vitiportelada@gmail.com>
2023-08-26 19:33:03 -0300

    First Release of the Hello World

---
+++ index.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("This is my first file in my git repository!");
+        boolean git = true;
+
+        System.out.print("Isso é um repositório git sim ou não? ");
+        String gitFinal = scanner.nextLine();
+
+        if (gitFinal.equals("sim")) {
+            System.out.println("Resposta Correta!");
+        } else {
+            System.out.println("Resposta Errada!");
+        }
+    }
+}
